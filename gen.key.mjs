46da16cee8dc54abc7d075b2c378f32642333455
Rapheal Chizitere <www.rashyking05@gmail.com>
2025-02-03 14:10:17 +0100

    chore: remove node-forge

--- gen.key.mjs
+++
@@ -1,7 +0,0 @@
-import forge from "node-forge";
-
-const key = forge.pki.rsa.generateKeyPair({ bits: 4096 });
-const publicKey = forge.pki.publicKeyFromPem(key.publicKey);
-const privateKey = forge.pki.publicKeyFromPem(key.privateKey);
-
-console.log({ publicKey, privateKey });
